fac22a0315c186133224ade24744ab49281a093e
Aprita Primayuda <aprita.primayuda@gmail.com>
2024-03-26 20:59:49 +0700

    Adding analyze route

--- index.js
+++ index.js
@@ -15,5 +15,9 @@ app.get('/about', (req, res) => {
   res.send('This is my about route..... ')
 })
 
+app.get('/analyze', (req, res) => {
+  res.send('This is my analize route for later connect to Gemini AI ')
+})
+
 // Export the Express API
 module.exports = app
